91be77627878412aab42d8be27ee5bdf80d8df71
adhe7x <105721068+adhe7x@users.noreply.github.com>
2022-06-12 22:35:04 +0700

    Edited by Adhi Prasetyo

--- promise.js
+++ promise.js
@@ -1,7 +1,33 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = async(emosi) => {
+  try {
+    
+    let rasa = null;
+
+    const bioskopIXX = await promiseTheaterIXX();
+    const bioskopVGC = await promiseTheaterVGC();
+
+    bioskopIXX.forEach(arr => {
+      if(arr.hasil == emosi) {
+        rasa += 1;
+      };
+    });
+
+    bioskopVGC.forEach(arr => {
+      if(arr.hasil == emosi) {
+        rasa += 1;
+      };
+    });
+
+    return rasa;
+
+  } catch (err) {
+    console.log(`Terjadi Kesalahan! ${err}`);
+  };
+};
 
 module.exports = {
   promiseOutput,
